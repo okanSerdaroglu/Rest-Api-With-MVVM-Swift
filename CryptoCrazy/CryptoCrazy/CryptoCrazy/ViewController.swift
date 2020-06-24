9188c6485f029dbdc34336ef40940078bbcb5139
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-24 22:33:31 +0300

    initial commit

---
+++ CryptoCrazy/CryptoCrazy/CryptoCrazy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCrazy
+//
+//  Created by Okan Serdaroğlu on 24.06.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
